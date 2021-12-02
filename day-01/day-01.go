23b61450bf04a74030fee36130c7bc84d13d4891
James Carman <jcarman@callibrity.com>
2021-12-02 06:05:45 -0500

    Finished day 1

--- day-01/day-01.go
+++ day-01/day-01.go
@@ -8,26 +8,47 @@ import (
 
 func main() {
 	lines, _ := input.ReadLines("./input.txt")
+	fmt.Println(calculatePart1(lines))
+	fmt.Println(calculatePart2(lines))
+}
 
-	var ints []int
+func calculatePart2(lines []string) int {
+	return countIncreases(toWindows(toDepths(lines)))
+}
 
-	for _,line := range lines {
-		i,_ := strconv.Atoi(line)
-		ints = append(ints, i)
-	}
+func calculatePart1(lines []string) int {
+	return countIncreases(toDepths(lines))
+}
 
+func countIncreases(values []int) int {
 	var count = 0
 
-	for ndx,curr := range ints {
+	for ndx, curr := range values {
 		if ndx > 0 {
-			prev := ints[ndx-1]
+			prev := values[ndx-1]
 			if curr > prev {
 				count = count + 1
 			}
 		}
 	}
-	fmt.Println(count)
-
+	return count
 }
 
+func toWindows(depths []int) []int {
+	var windows []int
+	for ndx, depth := range depths {
+		if ndx >= 2 {
+			windows = append(windows, depth+depths[ndx-1]+depths[ndx-2])
+		}
+	}
+	return windows
+}
 
+func toDepths(lines []string) []int {
+	var depths []int
+	for _, line := range lines {
+		i, _ := strconv.Atoi(line)
+		depths = append(depths, i)
+	}
+	return depths
+}
